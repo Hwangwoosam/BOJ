8e4b5bf3034d2a49c5d153b610560fa681ed3555
Hwangwoosam <sam1783@naver.com>
2024-07-02 12:46:09 +0900

    세훈이의 선물

---
+++ 17225.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int a,b,n;
+priority_queue<pair<int,char>,vector<pair<int,char>>,greater<pair<int,char>>> pq;
+
+int main(){
+    cin >> a >> b >> n;
+
+
+    int maxR = -1, maxB = -1;
+    for(int i = 0; i < n; i++){
+        int t,m;
+        char c;
+        cin >> t >> c >> m;
+
+        int cnt = 0;
+        if(c == 'B'){
+            if(maxB > t)  t = maxB;
+            for(int j = t; cnt < m; j += a){
+                pq.push({j,c});
+                cnt += 1;
+            }
+            maxB = t +m*a;
+        }else{
+            if(maxR > t)  t = maxR;
+            for(int j = t; cnt < m; j += b){
+                pq.push({j,c});
+                cnt += 1;
+            }
+            maxB = t +m*b;
+        }   
+    }
+
+    vector<int> ans1,ans2;
+    int idx = 1;
+    while(!pq.empty()){
+        char c = pq.top().second;
+        if(c == 'B'){
+            ans1.push_back(idx++);
+        }else{
+            ans2.push_back(idx++);
+        }
+        pq.pop();
+    }
+
+    int s1 = ans1.size();
+
+    cout << s1 << "\n";
+    for(int i = 0; i < s1; i++){
+        cout << ans1[i] << ' ';
+    }
+    cout << "\n";
+
+    int s2 = ans2.size();
+
+    cout << s2 << "\n";
+    for(int i = 0; i < s2; i++){
+        cout << ans2[i] << ' ';
+    }
+    cout << "\n";
+
+
+    return 0;
+}
