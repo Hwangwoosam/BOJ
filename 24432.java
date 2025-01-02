770546d52a0d124ef996fcd611553abb09bcf870
Hwangwoosam <sam1783@naver.com>
2025-01-02 10:02:40 +0900

    24432번 카드놀이

--- 24432.java
+++ 24432.java
@@ -82,14 +82,14 @@ class Main{
         int right = arr.size() - 1;
 
         while (left < right) {
-            int mid = left + (right - left + 1) / 2;
+            int mid = (left + right) / 2;
             if (arr.get(mid) <= target) {
-                left = mid;
+                left = mid + 1;
             } else {
-                right = mid - 1;
+                right = mid;
             }
         }
 
-        return left;
+        return left - 1;
     }
 }
