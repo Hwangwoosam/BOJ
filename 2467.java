2b2f6952b4742682089a2084594dd12fe3e6fbe2
Hwangwoosam <sam1783@naver.com>
2025-01-24 23:53:33 +0900

    2467번 용액

--- 2467.java
+++ 2467.java
@@ -3,24 +3,24 @@ import java.io.*;
 
 class Main{
     static int n;
-    static int [] num;
+    static Long [] num;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
 
-        num = new int[n];
+        num = new Long[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) num[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++) num[i] = Long.parseLong(st.nextToken());
 
         int left = 0, right = n-1;
         long min = Long.MAX_VALUE;
         int minL = 0, minR = 0;
         while(left < right){
-            long sum = Math.abs(num[left] + num[right]);
-            if(min > sum) {
-                min = sum;
+            long sum = num[left] + num[right];
+            if(min > Math.abs(sum)) {
+                min = Math.abs(sum);
                 minL = left;
                 minR = right;
             }
@@ -32,5 +32,3 @@ class Main{
         System.out.println(num[minL] + " " + num[minR]);
     }
 }
-
-
